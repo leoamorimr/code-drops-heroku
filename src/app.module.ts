b6d8aa1cc8c2281d6044e8e62d7f86d814de05dd
leonardo.a.ramos <leoamorimr@gmail.com>
2022-02-06 16:07:50 +0000

    Changing default port

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,13 @@ import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://dbUser:OBubPMXP8vQJcNYC@cluster0.i4kao.mongodb.net/test')],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://dbUser:OBubPMXP8vQJcNYC@cluster0.i4kao.mongodb.net/test',
+    ),
+  ],
   controllers: [],
-  providers: []
+  providers: [],
 })
 export class AppModule { }
