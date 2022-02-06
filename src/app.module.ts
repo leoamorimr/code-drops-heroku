b28b81be7f7521f741a552d3bec0fcfffa0f40e7
leonardo.a.ramos <leoamorimr@gmail.com>
2022-02-06 15:53:14 +0000

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [UsersModule, MongooseModule.forRoot('mongodb+srv://dbUser:OBubPMXP8vQJcNYC@cluster0.i4kao.mongodb.net/test')],
+  controllers: [],
+  providers: []
+})
+export class AppModule { }
